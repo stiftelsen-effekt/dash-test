ef83994707ddd937f846717026dbdba92752aebf
Trym Haddal <trymhaddal@gmail.com>
2024-01-25 19:32:24 +0100

    keynumbers changed column width

--- pages/keynumbers.py
+++ pages/keynumbers.py
@@ -14,16 +14,16 @@ layout = html.Div(
             dbc.Col([
                 html.Div([len(df['Donor_ID'].unique())], style=number_style),
                 html.Div(['Unike givere'], style={'text-align': 'center'})
-            ], width=4),
+            ], width=3),
             dbc.Col([
                 html.Div([f'{df["Sum_confirmed"].sum():,.0f} kr'], style=number_style),
                 html.Div(['Donert'], style={'text-align': 'center'})
             #], width=4, style={'border-left': '2px solid black','border-right': '2px solid black'}),
-            ], width=4),
+            ], width=6),
             dbc.Col([
                 html.Div([len(df.index)], style=number_style),
                 html.Div(['Donasjoner'], style={'text-align': 'center'})
-            ], width=4)
+            ], width=3)
         ], justify="center")
     ], fluid=True, style={"width": "100%", "max-width":"1000px", 'margin':0})
 )
