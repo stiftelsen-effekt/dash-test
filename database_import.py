f65e94fcfdc249bb0a3552a03e8aad68cfc27f08
Håkon Harnes <account@harnes.me>
2024-02-15 17:39:01 +0100

    Adds a simple caching mechanism for dataframes

--- database_import.py
+++ database_import.py
@@ -14,7 +14,22 @@ else:
     database = 'EffektAnalysisDB'
     engine = db.create_engine(f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{host}/{database}")
 
+# Initialize a cache dictionary
+cache = {}
+
 def get_df(table_name):
-    return pd.read_sql_table(table_name=table_name,con=engine.connect())
+    current_time = datetime.now()
+    
+    # Check if the table_name is in the cache and if the cache is still valid
+    if table_name in cache and (current_time - cache[table_name]['timestamp']) < timedelta(hours=12):
+        return cache[table_name]['data']
+    else:
+        # If the data is not in cache or cache is old, read from the database
+        df = pd.read_sql_table(table_name=table_name, con=engine.connect())
+        
+        # Update the cache
+        cache[table_name] = {'data': df, 'timestamp': current_time}
+        return df
+
 
 
